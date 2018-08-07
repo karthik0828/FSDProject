aaa2927643a0ccf471f963fe2bf2c24326bd86c1
karthik t <karthik.atmurthy@gmail.com>
2018-08-08 01:00:37 +0530

    Adding client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ViewTasksComponent } from './view-tasks/view-tasks.component';
+import { TaskMngService } from './service/task-mng.service';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { TaskSearchPipe } from './task-search.pipe';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes : Routes = [
+  {path: '', pathMatch: 'full',component: HomeComponent},
+  {path:'home' , component:HomeComponent},
+  {path:'viewtasks' ,component:ViewTasksComponent},
+  {path:'addtask' ,component:AddTaskComponent},
+  {path:'edittask/:id' , component:EditTaskComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewTasksComponent,   
+    AddTaskComponent,
+    EditTaskComponent,
+    TaskSearchPipe,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [TaskMngService], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
